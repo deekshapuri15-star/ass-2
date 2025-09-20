3910656c0353f9b533a7de32abf2c6040cdbc033
deeksha puri <deekshapuri15@gmail.com>
2025-09-21 01:23:26 +0530

    Add files via upload

---
+++ 2Q7.c
@@ -0,0 +1,10 @@
+   #include <stdio.h>
+
+    int main() {
+    int age;
+    printf("enter the age:", age);
+    scanf("%d",&age);
+    (age >=18 )?printf("Yes"):printf("no");
+    return 0;
+
+    }
